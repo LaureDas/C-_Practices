940aeb80567973c523610109b3edc92998ea59da
LaureDsy <laure.dassy@hotmail.com>
2021-03-01 14:09:39 +0100

    exercise 7 and 8 completed

--- Practice 1/practice1.cpp
+++ Practice 1/practice1.cpp
@@ -30,13 +30,81 @@ void print_start(int n){
     }
 }
 
+//exercise 6
+void print_five(){
+    int max=5;
+    int counter=0;
+    int i=0;
+    while(counter<max){
+        while(i<max){
+            cout<<"*";
+            i++;
+        }
+        cout<<endl;
+        i=0;
+        counter++;
+    }
+}
+
+void print_triangle(){
+    int max=5;
+    int i=0;
+    int counter=0;
+    while(i<max){
+        while(counter<i+1){
+            cout<<"*";
+            counter++;
+        }
+        cout<<endl;
+        i++;
+        counter=0;
+    }
+}
+
+void print_reverse_triangle(){
+    int max=5; 
+    int i=0;
+    int counter=5;
+    while (i<max){
+        while(counter>i){
+            cout<<"*";
+            counter--;
+        }
+        cout<<endl;
+        i++;
+        counter=5;
+    }
+}
+
+void max_value(int n){
+    double max= __DBL_MIN__;
+    double min= __DBL_MAX__;
+    double entry=0.0;
+    for(int i=0; i<n; i++){
+        cout<<"enter a number";
+        cin>>entry;
+        if(entry>max){
+            max=entry;
+        }
+        if(entry<min){
+            min=entry;
+        }
+    }
+    cout<<"maximum"<<max;
+    cout<<"minimum:"<<min;
+}
+
 int main(){
     int numberx, numbery, prints;
     //cout<<"Input two integers"<<endl;
     //cin>> numberx;
     //cin>> numbery;
     //multiple(numberx,numbery);
-    cin>>prints;
-    print_start(prints);
+    //cin>>prints;
+    //print_start(prints);
+    //print_five();
+    //print_triangle();
+    //print_reverse_triangle();
+    max_value(10);
     return 0;
 }
